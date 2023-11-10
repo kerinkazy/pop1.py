8fdc931654a1f8fd8ba95d380ac7ac9229026336
rellaxetw <mknmukashev@gmail.com>
2023-11-10 12:16:22 +0600

    uchunchu versia

---
+++ pop3.py
@@ -0,0 +1,10 @@
+name3 = 'Hello'
+print(name3.islower())
+
+name = 'Kerimkazy'
+print('startwith()')
+print(name.startswith('A'))
+
+print('strip()')
+name = 'Artur,Mirbek,Eleman'
+print(name.split(' '))
